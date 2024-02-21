105fb8e343311ef808483b8db120374e494e1a4f
llschall <llschall@gmx.com>
2024-02-21 22:57:45 +0100

    [Test] Create test material

---
+++ app/src/test/java/ardwloop/demo/DemoTest.java
@@ -0,0 +1,13 @@
+package ardwloop.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoTest {
+
+    @Test
+    public void testSetup() {
+        assertEquals(2,1+1);
+    }
+}

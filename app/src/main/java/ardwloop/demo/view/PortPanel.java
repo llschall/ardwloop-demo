ab36977a6ab08c2f1cf319ea5c78037319e0448c
llschall <llschall@gmx.com>
2024-11-24 20:17:03 +0100

    [Port] Highlight selected port

--- app/src/main/java/ardwloop/demo/view/PortPanel.java
+++ app/src/main/java/ardwloop/demo/view/PortPanel.java
@@ -4,6 +4,7 @@ import ardwloop.demo.model.CustomSelector;
 import org.llschall.ardwloop.serial.ArdwPortDescriptor;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.List;
@@ -33,10 +34,7 @@ class PortPanel extends JPanel {
         table.setFillsViewportHeight(true);
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         table.setSelectionBackground(Color.CYAN);
-
-        if (selection != -1) {
-            table.setRowSelectionInterval(selection, selection);
-        }
+        table.setDefaultRenderer(Object.class, new DemoCellRenderer(selection));
 
         JScrollPane pane = new JScrollPane(table);
 
@@ -48,7 +46,6 @@ class PortPanel extends JPanel {
 class DemoTableModel extends DefaultTableModel {
 
     final String[] colums = {"Valid", "Name", "Description", "System Name"};
-
 
     @Override
     public boolean isCellEditable(int row, int column) {
@@ -63,5 +60,26 @@ class DemoTableModel extends DefaultTableModel {
     @Override
     public String getColumnName(int column) {
         return colums[column];
+    }
+}
+
+class DemoCellRenderer extends DefaultTableCellRenderer {
+
+    final int selected;
+
+    public DemoCellRenderer(int selected) {
+        this.selected = selected;
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value,
+                                                   boolean isSelected, boolean hasFocus, int row, int column) {
+        JLabel label = new JLabel(value.toString());
+
+        if (row == selected) {
+            label.setBackground(Color.CYAN);
+            label.setOpaque(true);
+        }
+        return label;
     }
 }
